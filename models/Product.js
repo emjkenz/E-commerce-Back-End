a2c4f23e189966225efbb45d30a8b15f8e9e673f
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-05-21 21:47:00 +1000

    Updated ProductTag model

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,53 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      // Integer
+      type: DataTypes.INTEGER,
+      // Doesn't allow null values
+      allowNull: false,
+      // Set as primary key
+      primaryKey: true,
+      // Uses auto increment
+      autoIncrement: true,
+    },
+    product_name: {
+      // String
+      type: DataTypes.STRING,
+      // Doesn't allow null values
+      allowNull: false,
+    },
+    price: {
+      // Decimal
+      type: DataTypes.DECIMAL,
+      // Doesn't allow null values
+      allowNull: false,
+      // Validates that the value is a decimal
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      // Integer
+      type: DataTypes.INTEGER,
+      // Doesn't allow null values
+      allowNull: false,
+      // Set a default value of 10
+      defaultValue: 10,
+      // Validates that the value is numeric
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      // Integer
+      type: DataTypes.INTEGER,
+      // References the Category model's id
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
