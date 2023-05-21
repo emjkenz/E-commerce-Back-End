673db807c400e9d6cfd30d66e54f2d6f77c865fe
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-05-21 22:49:50 +1000

    Added categories routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,21 +14,64 @@ router.get('/', async(req, res) => {
   res.json(categories);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const products = await Product.findAll({
+    where: {
+      category_id: req.params.id
+    }
+  });
+  res.json(products);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  const create = Object.keys(req.body).length > 0 ? req.body : req.query;
+
+  // If id is provided, check if it exists
+  if (create.id) {
+    const cat = await Category.findOne({
+      where: {
+        id: create.id
+      }
+    });
+
+    if (cat) {
+      res.json({ status: "error", message: "Category already exists" });
+      return;
+    }
+  }
+
+  Category.create(create);
+  res.json({ status: "success" });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  const update = Object.keys(req.body).length > 0 ? req.body : req.query;
+
+  if (update.id) {
+    res.json({ status: "error", message: "Cannot update id" });
+    return;
+  }
+
+  Category.update(update, {
+    where: {
+      id: req.params.id
+    }
+  });
+  res.status(200).json({ status: 'success' });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  });
+  res.status(200).json({status:'success'});
 });
 
 module.exports = router;
