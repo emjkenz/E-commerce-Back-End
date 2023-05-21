6039f9cb47dce4fdef6372d57a1d3f74d32034c6
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-05-21 21:49:48 +1000

    Added route for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,15 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all categories
   // be sure to include its associated Products
+  const categories = await Category.findAll({
+    include: {
+      model: Product
+    }
+  });
+  res.json(categories);
 });
 
 router.get('/:id', (req, res) => {
