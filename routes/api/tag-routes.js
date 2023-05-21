9f2c338bc8adb3ab8bff54a32c65f7deb7a577bf
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-05-21 22:56:22 +1000

    Added some of the tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,29 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  const tags = await Tag.findAll({
+    include: {
+      model: Product
+    }
+  });
+  res.json(tags);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  const tags = await Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product
+    }
+  });
+  res.json(tags);
 });
 
 router.post('/', (req, res) => {
@@ -23,6 +38,12 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  });
+  res.status(200).json({ status: 'success' });
 });
 
 module.exports = router;
